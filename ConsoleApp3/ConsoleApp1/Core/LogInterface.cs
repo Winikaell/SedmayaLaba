aaaab47032f4cb1972bb5ddfcf3da552d0f62933
Winikaell <cxcwini@mail.ru>
2021-11-26 14:35:31 +0300

    Tretya

---
+++ ConsoleApp3/ConsoleApp1/Core/LogInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Core
+{
+    interface LogInterface
+    {
+        LogInterface log(string str);
+             LogInterface write();
+    }
+}
