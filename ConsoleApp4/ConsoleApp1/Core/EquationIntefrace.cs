b4d67324df1c65efc5a0eeb7a06c6b5a9588f312
Winikaell <cxcwini@mail.ru>
2021-11-26 14:35:47 +0300

    Chetvertaya

---
+++ ConsoleApp4/ConsoleApp1/Core/EquationIntefrace.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Core
+{
+    interface EquationInterface
+    {
+        List<float> solve(float a, float b, float c);
+    }
+}
---
+++ ConsoleApp4/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.Kapustin;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KapustinLog logger = KapustinLog.I();
+
+            Console.WriteLine("Введите a, b и с");
+
+            List<float> arg = new List<float>(3);
+
+            for (int i = 0; i < 3; i++)
+            {
+                arg.Add(float.Parse(Console.ReadLine()));
+            }
+
+            logger.log($"{arg[0]}x^2 + {arg[1]}x + {arg[2]}");
+
+            A b = new A();
+
+            try
+            {
+                logger.log("Корни уравнения: " + string.Join(", ", b.solve(arg[0], arg[1], arg[2]).ToArray()));
+            }
+            catch (KapustinException e)
+            {
+                logger.log(e.Message);
+            }
+
+            logger.write();
+            Console.ReadKey();
+
+        }
+    }
+}
